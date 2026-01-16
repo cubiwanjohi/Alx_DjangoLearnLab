27cf7636b497e718d02c7f0c631c0a65b3e8b753
peter wanjohi <119779435+cubiwanjohi@users.noreply.github.com>
2026-01-16 14:27:33 +0300

    Update models.py

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
     publication_year = models.IntegerField()
-
-    def __str__(self):
-        return self.title
