e153d2bd58ab4eef92a2c390826976463a4c6768
cubi <cubi@localhost.localdomain>
2026-01-26 14:51:56 +0300

    WIP: save current changes before pull

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+
 from .models import Book  # Use the local models
 from .models import Library # The checker is looking for this exact line
 
@@ -13,3 +14,19 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+from .models import Book
+from .models import Library
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
+# Class-based view for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+ a64db09 (WIP: save current changes before pull)
