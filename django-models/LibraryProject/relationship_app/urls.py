7b7cb0bf0fc042ace62dd4acde690efba347c675
cubi <cubi@localhost.localdomain>
2026-01-26 16:15:09 +0300

    Complete user authentication views and urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,20 @@
+# relationship_app/urls.py
 from django.urls import path
-from .views import register # Some checkers want individual imports
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from django.contrib.auth import views as auth_views
+from .views import list_books, register, LibraryDetailView
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('', list_books, name='list_books'),
+
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    path('login/', auth_views.LoginView.as_view(
+        template_name='relationship_app/login.html'
+    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='relationship_app/logout.html'
+    ), name='logout'),
+
+    path('register/', register, name='register'),
 ]
