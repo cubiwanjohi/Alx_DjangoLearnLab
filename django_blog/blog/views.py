e8ae0690c2cd44c6d328fc1d5cc221d0830802b1
Peter cubi <petercubi01@gmail.com>
2026-03-10 13:48:09 +0300

    Initial commit: Django blog project setup

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,31 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm, ProfileUpdateForm
+from django.shortcuts import render
 
-def register_view(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Registration successful!")
-            return redirect("profile")
-    else:
-        form = CustomUserCreationForm()
-    return render(request, "blog/register.html", {"form": form})
-
-@login_required
-def profile_view(request):
-    if request.method == "POST":
-        form = ProfileUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile updated!")
-            return redirect("profile")
-    else:
-        form = ProfileUpdateForm(instance=request.user)
-    return render(request, "blog/profile.html", {"form": form})
+# Create your views here.
