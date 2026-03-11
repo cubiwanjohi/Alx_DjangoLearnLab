15d48ff54d01bed32ef50aa1a7d0ad5c6fceb70a
Peter cubi <petercubi01@gmail.com>
2026-03-11 09:32:09 +0300

    fix to deploy

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path
+from .views import (
+    PostListView, PostDetailView, PostCreateView,
+    PostUpdateView, PostDeleteView
+)
 
-urlpatterns = [
+urlpatterns += [
+    urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
